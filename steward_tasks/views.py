a1913f9b47f6fa912b3d49e6dc3b136419d76ba4
Steven Arcangeli <steven@highlig.ht>
2013-11-08 23:57:58 -0800

    Adding some missing docstrings

--- steward_tasks/views.py
+++ steward_tasks/views.py
@@ -7,23 +7,27 @@ from . import celery
 
 @view_config(route_name='tasks_active', renderer='json')
 def do_active(request):
+    """ Get all active tasks by worker """
     inspect = Inspect(app=celery)
     return inspect.active()
 
 
 @view_config(route_name='tasks_registered', renderer='json')
 def do_registered(request):
+    """ Get a list of all registered tasks """
     inspect = Inspect(app=celery)
     return inspect.registered()
 
 
 @view_config(route_name='tasks_reserved', renderer='json')
 def do_reserved(request):
+    """ Get all tasks that have been reserved but are not executing """
     inspect = Inspect(app=celery)
     return inspect.reserved()
 
 
 @view_config(route_name='tasks_scheduled', renderer='json')
 def do_scheduled(request):
+    """ Get a list of all tasks waiting to be scheduled """
     inspect = Inspect(app=celery)
     return inspect.scheduled()
