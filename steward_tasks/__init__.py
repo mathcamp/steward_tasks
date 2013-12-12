465c71ff9349fe69554078f27e1bc92e606be8d6
Steven Arcangeli <steven@highlig.ht>
2013-12-11 16:33:35 -0800

    Revert "Set the config registry onto the task object"
    This reverts commit eb4dbbdcfd4d36428bad0f9dd82aa37d5fe5db26.

--- steward_tasks/__init__.py
+++ steward_tasks/__init__.py
@@ -62,7 +62,6 @@ class BaseStewardTask(Task):  # pylint: disable=W0223
     abstract = True
     config = None
     callbacks = []
-    registry = None
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
         for callback in self.callbacks:
@@ -227,7 +226,6 @@ def init_celery(conf_file):
         if hasattr(mod, 'include_tasks'):
             mod.include_tasks(config)
 
-    BaseStewardTask.registry = config.registry
     StewardTask = type('StewardTask', tuple(config.mixins + [
                        BaseStewardTask]), {'abstract': True, 'config': config})
 
